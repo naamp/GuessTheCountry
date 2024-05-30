cabf3853496485d722559d769579a25a4f9615ba
JanisKramer <janis.kramer@students.fhnw.ch>
2024-05-30 13:08:03 +0200

    20240530_1_Janis
    Geojson in Karte hinterlegt

--- client/src/Mappage.js
+++ client/src/Mappage.js
@@ -5,29 +5,29 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 import { MapContainer, TileLayer, useMap, Popup, Marker, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import countriesGeoJSON from './geodata/custom.geo';
 
 
 const Mappage = () => {
 
-
   return (
-
     <div className="App">
-  <MapContainer           
-    center={[46,7]}
-    zoom={3}
-    scrollWheelZoom={true}>
-    <TileLayer
-        attribution='&copy; <a href="https://www.esri.com/en-us/home">Esri</a>, USGS, NOAA'
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}"
-    />
-    <Marker position={[46,7]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-
+      <MapContainer
+        center={[46, 7]}
+        zoom={3}
+        scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.esri.com/en-us/home">Esri</a>, USGS, NOAA'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}"
+        />
+        <Marker position={[46, 7]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        {/* Add GeoJSON layer with onEachFeature option */}
+        <GeoJSON data={countriesGeoJSON} />
+      </MapContainer>
     </div>
   );
 };
