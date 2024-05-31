d7310f6f3e4d63d29b87aeb8da14c0104f6d145a
Micha Tschanz <micha.tschanz@students.fhnw.ch>
2024-05-31 14:38:06 +0200

    20240531_9_Micha
    CSS update

--- client/src/info-popup.js
+++ client/src/info-popup.js
@@ -20,12 +20,12 @@ return (
         className="modal"
         overlayClassName="modal-overlay"
       >
-        <h2>Game Info</h2>
-        <h3>Developers:</h3>
+        <h1 className="ueberschrift">Game Info</h1>
+        <h3 className="ueberschrift">Developers:</h3>
         <p>Amport Nando, Kalbermatten Pascal, Kramer Janis, Tschanz Micha, Uythoven Sven</p>
-        <h3>Geodata:</h3>
+        <h3 className="ueberschrift">Geodata:</h3>
         <a href="https://geojson-maps.kyd.au/">https://geojson-maps.kyd.au/</a>
-        <h3>Impressum:</h3>
+        <h3 className="ueberschrift">Impressum:</h3>
         <p>The developers accepts no responsibility for the accuracy and currency of the content on <a href="http://geogame-guessthecountry.vercel.app">http://geogame-guessthecountry.vercel.app</a>.
             The developers provides no guarantee for the operation of the <a href="http://geogame-guessthecountry.vercel.app">http://geogame-guessthecountry.vercel.app</a> website and accepts no liability for any damage to hardware or software caused by viruses or technical problems of any kind. Access to <a href="http://geogame-guessthecountry.vercel.app">http://geogame-guessthecountry.vercel.app</a> is at the user's own risk and responsibility.
             When using the website provisions of foreign law may be violated; in such cases, the developers rejects any liability. Links to external websites on this website serve merely as a reference to topics that could be helpful for users of this site. The developers have no control over the content of external links, which is why the developers accept no liability for the content of such external links, including their accuracy, completeness, reliability or suitability for specific purposes. Illegal content will be removed immediately upon notification.</p>
